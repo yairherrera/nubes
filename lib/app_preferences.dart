b1f98b9857d9c2e8ae30d6b3213922d03fedad92
yairherrera <yair.herrera@tecsup.edu.pe>
2022-12-06 18:08:26 -0500

    Navegacion

--- lib/app_preferences.dart
+++ lib/app_preferences.dart
@@ -8,19 +8,23 @@ import 'package:http/http.dart' as http;
 import 'models.dart';
 
 class ApiClient {
+  //url que conecta a la api
   static const baseUrl = "https://api.coingecko.com/api/v3/";
 
+  //Establecemos comunicacion con la api mediante parametros
   Future<Coin> getCoin(String coinId, {bool localization = false}) async {
     final response = await http.get(Uri.parse(
         "${baseUrl}coins/$coinId?localization=$localization&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false"));
-
+    //Esperamos respuesta de la api
     if (response.statusCode == 200) {
       return Coin.fromJson(jsonDecode(response.body));
+      //Si sale mal nos tira un error
     } else {
       throw Exception("Error getting Coin");
     }
   }
 
+  //Obtencion de datos diariamente
   Future<MarketChart> getCoinMarketChartInfo(String coinId) async {
     String days = "30";
     String interval = "daily";
@@ -34,6 +38,7 @@ class ApiClient {
     }
   }
 
+  //Datos de una moneda en especifico
   Future<CoinMarketInfo> getCoinMarketInfo(String coinId) async {
     final response = await http.get(Uri.parse(
         "${baseUrl}coins/$coinId?localization=false&tickers=false&market_data=true&community_data=true&developer_data=true&sparkline=false"));
@@ -45,7 +50,9 @@ class ApiClient {
     }
   }
 
+  //Realizamos un bucle de una moneda obteniendo el id en especial
   Future<List<Coin>> getCoins(List<String> coinsIds) async {
+    //Se coloca dentro del array coins
     List<Coin> coins = [];
 
     for (String id in coinsIds) {
